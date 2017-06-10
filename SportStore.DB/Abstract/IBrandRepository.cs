ed693348868f6fd6707b4b55474656f6e9ca1ce5
Rokanuzzaman <rokan08@gmail.com>
2017-06-10 13:15:12 -0400

    Brand Repository Interface added Repository Created

---
+++ SportStore.DB/Abstract/IBrandRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportStore.Domain.Entities;
+
+namespace SportStore.DB.Abstract
+{
+    public interface IBrandRepository
+    {
+        IList<Brand> GetAll();
+    }
+}
---
+++ SportStore.DB/Repository/BrandRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportStore.DB.Abstract;
+using SportStore.Domain.Entities;
+
+namespace SportStore.DB.Repository
+{
+    public class BrandRepository:IBrandRepository
+    {
+       
+       
+        public IList<Brand> GetAll()
+        {
+            using (SportStoreContext db = new DB.SportStoreContext())
+            {
+                return db.Brands.ToList();
+            }
+        }
+    }
+}
