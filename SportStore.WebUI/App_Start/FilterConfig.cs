dd5980201fd06f80de24ed853332b252f6f22bda
Rokanuzzaman <rokan08@gmail.com>
2017-06-10 12:50:42 -0400

    changed a lot

---
+++ SportStore.WebUI/App_Start/FilterConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportStore.WebUI.App_Start
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilter(GlobalFilterCollection filter) {
+            filter.Add(new HandleErrorAttribute());
+            filter.Add(new AuthorizeAttribute());
+        }
+    }
+}
--- SportStore.WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
+++ SportStore.WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using SportStore.WebUI.Abstract;
-
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SportStore.WebUI.Infrastructure.Concrete
 {
@@ -19,6 +20,31 @@ namespace SportStore.WebUI.Infrastructure.Concrete
                 FormsAuthentication.SetAuthCookie(username, false);
 
             return result;
+        }
+
+        public byte[] GetByteArray(string message) {
+            return Encoding.UTF8.GetBytes(message);
+        }
+
+        public string CreateSalt(int size) {
+
+            var cryptoServiceProvider = new RNGCryptoServiceProvider();
+            var buffer = new byte[size];
+            cryptoServiceProvider.GetBytes(buffer);
+
+            return Convert.ToBase64String(buffer);
+        }
+
+        public string GetPasswordHashAndSalt(string password) {
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            byte[] byteData = GetByteArray(password);
+            byte[] resultBytes = sha.ComputeHash(byteData);
+
+            return GetString(resultBytes);
+        }
+
+        public string GetString(byte[] byteData) {
+            return Convert.ToBase64String(byteData);
         }
     }
 }
